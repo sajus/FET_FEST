fe5a8bbfefc478c8cbb3b98a595778b646a6b8b6
JaydeepTank2 <jaydeep.tank@gmail.com>
2019-06-14 22:35:29 +0530

    Adding base style variables

---
+++ style-dist/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+// Do not edit directly
+// Generated on Fri, 14 Jun 2019 16:58:03 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
---
+++ style-dist/build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+// Do not edit directly
+// Generated on Fri, 14 Jun 2019 16:58:03 GMT
+//
+
+#import "StyleDictionarySize.h"
+
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
