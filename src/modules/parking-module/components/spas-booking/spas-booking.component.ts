53da43604f27f2c9019cc52965cad74c40bb3023
omkarpadwale <padwale.om@gmail.com>
2019-06-15 11:17:15 +0530

    added basic component structure

---
+++ src/modules/parking-module/components/spas-booking/spas-booking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spas-booking',
+  templateUrl: './spas-booking.component.html',
+  styleUrls: ['./spas-booking.component.scss']
+})
+export class SpasBookingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
